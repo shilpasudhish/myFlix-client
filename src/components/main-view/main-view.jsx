be6f3a1d915327ef0b2145419c8d50324125e661
shilpasudhish <bindultali@gmail.com>
2024-11-17 15:12:43 +0100

    Implemented API fetch for movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,37 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../MovieView/movie-view";
 
+
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Bloody Title",
-      image: "https://media.gettyimages.com/id/1332479704/vector/viking-warrior-poster.jpg?s=612x612&w=0&k=20&c=9zxo1XYHh0P4jd5xnMg96R9rhPwvczGHKil5Rj45hsY=",
-      author: "Marijn Haverbeke"
-    },
-    {
-      id: 2,
-      title: "Pirates of carribean",
-      image: "https://media.gettyimages.com/id/458587791/photo/pirates-of-the-caribbean-on-stranger-tides-poster.jpg?s=612x612&w=0&k=20&c=61W2D7oJTn7h9lumx1bKO0vhzj1JzSb7mnCCYYc6Bxk=",
-      author: "Federico Kereki"
-    },
-    {
-      id: 3,
-      title: "Horror",
-      image: "https://media.gettyimages.com/id/1768286889/vector/slasher-horror-poster.jpg?s=612x612&w=0&k=20&c=eI79ClMmOrDYV0UCXUWCVcwX-_lhsQIBNNVvibFIHNY=",
-      author: "David Flanagan"
-    },
-    {
-      id: 4,
-      title: "The Godfather",
-      image:
-        "https://media.gettyimages.com/id/1137152399/photo/the-godfather.jpg?s=1024x1024&w=gi&k=20&c=5QWLqzlBAI-5WV01UQoV21o3LsmRu_zGPQYbp54xINs=",
-      author: "Robin Wieruch"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const urlAPI = "https://jsonplaceholder.typicode.com/posts";
+
+  useEffect(() => {
+    fetch(urlAPI, [])
+    .then((response => response.json()))
+    .then ((data) => {
+      console.log(data);
+      setMovies(data);
+
+    } 
+  )
+  .catch((error) => console.error('Error fetching movies:', error));
+  },[]
+  
+)
 
   if (selectedMovie) {
     return (
